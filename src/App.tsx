5428d37d0fcce482b17c34153d218f0ac3fe6854
Julie Andersson <juan2301@student.miun.se>
2025-05-02 13:48:04 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <>
+    <h1>Projekt</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Startsida</h1>
+    </div>
+  )
+}
+
+export default HomePage
