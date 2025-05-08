c40f63d888df69cda5edbc6bb6fa94f77ba03629
Julie Andersson <juan2301@student.miun.se>
2025-05-08 16:41:16 +0200

    stylar footer i footer.css och inline css, matchar med header

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,10 @@
+import "./css/Footer.css";
 
 function Footer() {
   return (
     <footer>
-        <p>Recensera Mera <br />
+        <p style={{ fontWeight: "bold", fontSize: "1.1rem", color: "#e2c65a" }}>&copy; 2025 Recensera Mera</p>
+        <p>Projekt DT210G - Fördjupad Frontend-utveckling <br/>
           Av Julie Andersson</p>
     </footer>
   )
